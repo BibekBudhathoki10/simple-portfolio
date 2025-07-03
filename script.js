a94029aec4b1c7e5749e486f3f2c70f2000379e9
aashishbudhathoki10 <ashishbudhathoki354@gmail.com>
2025-07-03 11:33:29 +0545

    Simple Portfolio Using HTML,CSS & JS

---
+++ script.js
@@ -0,0 +1,67 @@
+
+class Portfolio {
+    constructor(name, age, aim, education, institution, facebook, github, linkedin) {
+        this.name = name;
+        this.age = age;
+        this.aim = aim;
+        this.education = education;
+        this.institution = institution;
+        this.facebook = facebook;
+        this.github = github;
+        this.linkedin = linkedin;
+    }
+
+    displayInfo() {
+        return `
+<span class="code-keyword">const</span> <span class="variable">name</span> = <span class="value">"${this.name}"</span>;
+<span class="code-keyword">const</span> <span class="variable">age</span> = <span class="value">${this.age}</span>;
+<span class="code-keyword">const</span> <span class="variable">aim</span> = <span class="value">"${this.aim}"</span>;
+<span class="code-keyword">const</span> <span class="variable">education</span> = <span class="value">"${this.education}"</span>;
+<span class="code-keyword">const</span> <span class="variable">institution</span> = <span class="value">"${this.institution}"</span>;
+
+<span class="comment">// Displaying Information</span>
+<span class="console-keyword">console</span>.<span class="function-call">log</span>(<span class="value">"Welcome to Bibek Budhathoki's Portfolio!"</span>);
+<span class="console-keyword">console</span>.<span class="function-call">log</span>(<span class="value">"Name: "</span> + <span class="variable">name</span>);
+<span class="console-keyword">console</span>.<span class="function-call">log</span>(<span class="value">"Age: "</span> + <span class="variable">age</span>);
+<span class="console-keyword">console</span>.<span class="function-call">log</span>(<span class="value">"Aim: "</span> + <span class="variable">aim</span>);
+<span class="console-keyword">console</span>.<span class="function-call">log</span>(<span class="value">"Education: "</span> + <span class="variable">education</span>);
+<span class="console-keyword">console</span>.<span class="function-call">log</span>(<span class="value">"Institution: "</span> + <span class="variable">institution</span>);
+        `;
+    }
+
+    displaySocialLinks() {
+        return `
+Connect with me:
+<a href="${this.facebook}" target="_blank">Facebook</a>
+<a href="${this.github}" target="_blank">GitHub</a>
+<a href="${this.linkedin}" target="_blank">LinkedIn</a>
+        `;
+    }
+}
+
+
+const myPortfolio = new Portfolio(
+    "Bibek Budhathoki",
+    20,
+    "Full Stack Developer (Java)",
+    "BSC Hons. Computing (BIT)",
+    "Itahari International College - London Metropolitan University",
+    "https://www.facebook.com/ywf.ashish",
+    "https://github.com/BibekBudhathoki10",
+    "https://www.linkedin.com/in/bibek-budhathoki-91a19330b",
+);
+
+document.getElementById('terminal-content').innerHTML = myPortfolio.displayInfo();
+
+document.getElementById('social-links').innerHTML = myPortfolio.displaySocialLinks();
+
+const copyBtn = document.getElementById('copy-btn');
+
+copyBtn.addEventListener('click', () => {
+    const terminalContent = document.getElementById('terminal-content').innerText;
+    navigator.clipboard.writeText(terminalContent).then(() => {
+        alert("Code copied to clipboard!");
+    }).catch(err => {
+        console.error("Failed to copy code: ", err);
+    });
+});
